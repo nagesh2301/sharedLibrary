fc4fb767b95810ad19e796313d5527b1f901f7da
nagesh2301 <115499779+nagesh2301@users.noreply.github.com>
2023-07-17 00:15:41 +0530

    Update scripMulti.groovy

--- vars/scripMulti.groovy
+++ vars/scripMulti.groovy
@@ -1,1 +1,20 @@
 
+def newGit(repo)
+{
+  git "${repo}"
+}
+
+def newMaven()
+{
+  sh "mvn package"
+}
+
+def newDeploy(workspace,ip,context)
+{
+  sh "scp ${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/{context}.war"
+}
+
+def newTesting(workspace)
+{
+  sh "java -jar ${workspace}/testing.jar"
+}
