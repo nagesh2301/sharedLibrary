ffb5025589b6ed70d10d17045b90da8cfc12c9e5
nagesh2301 <115499779+nagesh2301@users.noreply.github.com>
2023-07-16 18:13:37 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,7 +13,7 @@ def newDeploy(jobname,ip,context)
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
-def newTesting(repo)
+def newTesting(repo,jobname)
 {
   git "${repo}"
   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
